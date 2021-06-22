12032734c493263ecabb28762d2379ed3e237958
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-06-22 23:55:09 +0300

    Create help.js

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
 exports.run = (client, message) => {
   const Embed = new Discord.MessageEmbed()
-  .setColor("BLUE")
-  .setTimestamp()
+  .setColor("RANDOM")
   .setDescription(`this is all command gifs`)
-  .addField("**Gifs**","`mangif`, `womangif`, `couplegif`, `babygif`, `animegif`, `animalgif`, `randomgif`, `randompp`")
+  .addField("**Info**","`help`, `ping`, `invite`")
+  .addField("**Gifs**","``, ``, ``, ``")
   message.channel.send(Embed)
 }
 exports.conf = {
