d39105dde5c3b2dd241be86f436ee0cd6185f911
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-06-22 23:33:42 +0300

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,1 +1,19 @@
+const Discord = require('discord.js');
+exports.run = (client, message) => {
+  const Embed = new Discord.MessageEmbed()
+  .setColor("BLUE")
+  .setTimestamp()
+  .setDescription(`[ Invite Link ](bot link)`)
+  
+  message.channel.send(Embed)
+}
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["invitelink"]
+};
+
+exports.help = {
+  name: 'invite'
+};
 
